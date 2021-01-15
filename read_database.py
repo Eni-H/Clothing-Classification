9b5a3e0024d3b10ad1db86618a8d0cbbf61d290c
Eni-H <e_hysenllari@yahoo.com>
2021-01-15 13:08:19 +0100

    change variable names to python style

--- read_database.py
+++ read_database.py
@@ -3,7 +3,7 @@ import gzip
 import numpy as np
 import matplotlib.pyplot  as plt
 
-def readDatabase(path, kind='train'):
+def read_database(path, kind='train'):
     
     """Load MNIST Fashion data from `path`"""
 
@@ -25,24 +25,23 @@ def readDatabase(path, kind='train'):
     return images, labels
 
 
-
 if __name__ == "__main__":
     path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'data')
-    [trainData, trainLabels] = readDatabase(path)
-    [testData, testLabels] = readDatabase(path, 'test')
+    [train_data, train_labels] = read_database(path)
+    [test_data, test_labels] = read_database(path, 'test')
 
     """Test MNIST Fashion data """
     #labels map
-    labelsMap = {0:'T-shirt/top', 1:'Trouser', 2:'Pullover', 3:'Dress', 4:'Coat', 5:'Sandal', 6:'Shirt', 7:'Sneaker', 8:'Bag', 9:'Ankle boot'}
+    labels_map = {0:'T-shirt/top', 1:'Trouser', 2:'Pullover', 3:'Dress', 4:'Coat', 5:'Sandal', 6:'Shirt', 7:'Sneaker', 8:'Bag', 9:'Ankle boot'}
     # plot some images to make sure that the labels and images 
     fig, axarr = plt.subplots(2,2)
-    axarr[0,0].imshow(trainData[0])
-    axarr[0,0].set_title(labelsMap[trainLabels[0]])
-    axarr[0,1].imshow(trainData[1])
-    axarr[0,1].set_title(labelsMap[trainLabels[1]])
-    axarr[1,0].imshow(testData[0])
-    axarr[1,0].set_title(labelsMap[testLabels[0]])
-    axarr[1,1].imshow(testData[1])
-    axarr[1,1].set_title(labelsMap[testLabels[1]])
+    axarr[0,0].imshow(train_data[0])
+    axarr[0,0].set_title(labels_map[train_labels[0]])
+    axarr[0,1].imshow(train_data[1])
+    axarr[0,1].set_title(labels_map[train_labels[1]])
+    axarr[1,0].imshow(test_data[0])
+    axarr[1,0].set_title(labels_map[test_labels[0]])
+    axarr[1,1].imshow(test_data[1])
+    axarr[1,1].set_title(labels_map[test_labels[1]])
     plt.show()
 
